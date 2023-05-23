48ca033a925437791e1791920f6762cec6e7c7d5
Shreyas Srivastava <shsrivas@shsrivas-mn2.linkedin.biz>
2023-05-23 16:28:47 -0700

    debug

--- config/finetune_got.py
+++ config/finetune_got.py
@@ -18,7 +18,7 @@ always_save_checkpoint = False
 # shakespeare has 301,966 tokens, so 1 epoch ~= 9.2 iters
 batch_size = 1
 gradient_accumulation_steps = 32
-max_iters = 100
+max_iters = 30
 
 # finetune at constant LR
 learning_rate = 3e-5
